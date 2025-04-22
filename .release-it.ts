c42ebc540febaf0f7a5e1153f49bfbacaad93323
Mike Willbanks <me@mike.engineer>
2025-04-21 19:43:12 -0500

    chore: add publishConfig

--- .release-it.ts
+++ .release-it.ts
@@ -1,26 +1,28 @@
-import type { Config } from 'release-it';
+import type { Config } from "release-it";
 
 export default {
   git: {
     commit: true,
+    commitMessage: "chore: release v${version}",
+    push: true,
+    pushRepo: "origin",
+    requireBranch: "main",
+    requireCommits: true,
     tag: true,
-    push: true
   },
   github: {
-    release: true,
     comments: {
       submit: true,
     },
+    release: true,
   },
   npm: {
-    publish: true
+    publish: true,
   },
   plugins: {
-    '@release-it/conventional-changelog': {
-      preset: {
-        name: 'atom',
-      },
+    "@release-it/conventional-changelog": {
       infile: "CHANGELOG.md",
+      preset: "angular",
     },
-  }
+  },
 } satisfies Config;
